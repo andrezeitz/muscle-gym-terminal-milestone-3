cb3c6e2b6a84744ff712df89967b08f29e87fb1b
andrezeitz <swe_zeitz@hotmail.com>
2021-08-16 07:49:53 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-# python code goes here
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('muscle_gym')
+
+new_customers = SHEET.worksheet('new_customers')
+
+data = new_customers.get_all_values()
+
+print(data)
