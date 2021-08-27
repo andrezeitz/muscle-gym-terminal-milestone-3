a03af0b68d3f48293c4fcbd3551f1795da993393
andrezeitz <swe_zeitz@hotmail.com>
2021-08-27 12:39:44 +0000

    Add validator to all inputs inside start_menu_new_customer

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import gspread
 import math
 from google.oauth2.service_account import Credentials
 from pprint import pprint
+import re
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -24,9 +25,9 @@ def start_menu():
         user_decide = input(
         """
         If you want to registered as a new customer please press 1.
-        If you want to calculate your Basal Metabolic Rate (BMR) please press 2.
+        If you want to calculate the membership price on how many times you train please press 2.
         If you want to calculate your Body Mass Index (BMI) please press 3
-        If you want to calculate the membership price on how many times you train please press 4.
+        If you want to calculate your Basal Metabolic Rate (BMR) please press 4.
         If you are staff manager please press 5.
         """
         )
@@ -36,6 +37,7 @@ def start_menu():
     start_menu_calculate_bmr(user_decide)
     start_menu_calculate_bmi(user_decide)
     start_menu_calculate_membership(user_decide)
+    start_menu_manager(user_decide)
 
 
 def validate_start_menu(values):
@@ -54,6 +56,16 @@ def validate_start_menu(values):
     return True
 
 
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+
+def check(email):
+    if (re.search(regex,email)):
+        print("Valid Email")
+    else:
+        print("Invalid Email")
+
+        
 def start_menu_new_customer(values):
     """
     Will let the new customer enter all of there information
@@ -61,30 +73,32 @@ def start_menu_new_customer(values):
     if values == "1":
         new_customer = {}
 
-        data_firstname = input("Please provide us with your first name: ")
-        new_customer["firstname"] = data_firstname
-        print(f"Your first name is saved as {data_firstname}.\n")
+        while True:
+            data_name = input("Please provide us with your name in all caps: ")
+            if not data_name.isupper():
+                print("ERROR, please provide us with your name again")
+            else:
+                break
+        new_customer["firstname"] = data_name
+        print(f"Your first name is saved as {data_name}.\n")
 
-        data_lastname = input("Please provide us with your last name: ")
-        new_customer["lastname"] = data_lastname
-        print(f"Your last name is saved as {data_lastname}.\n")
-
-        data_address = input("Please provide us with your adress: ")
-        new_customer["address"] = data_address
-        print(f"Your adress is saved as {data_address}.\n")
-
-        data_zipcode = int(input("Please provide us with your zip-code: "))
-        new_customer["zipcode"] = data_zipcode
-        print(f"Your zip-code is saved as {data_zipcode}.\n")
-
-        data_phone = int(input("Please provide us with your phone number: "))
+        while True:
+            try:
+                data_phone = int(input("Please provide us with your phone number: "))
+            except ValueError:
+                print("ERROR, please provide us with your phone number again")
+                continue
+            else:
+                break
         new_customer["phone"] = data_phone
         print(f"Your phone number is saved as {data_phone}.\n")
 
-        data_email = input("Please provide us with your email adress: ")
-        new_customer["email"] = data_email
-        print(f"Your email adress is saved as {data_email}.\n")
-        return add_new_customer(new_customer)
+        if __name__ == "__main__":
+            email = input("Please provide us with your email adress: ")
+            check(email)
+            new_customer["email"] = email
+            print(f"Your email adress is saved as {email}.\n")
+            return add_new_customer(new_customer)
 
 
 def add_new_customer(new_customer):
@@ -97,16 +111,76 @@ def add_new_customer(new_customer):
     print("Worksheet updated successfully.")
 
 
+def validate_times_week(times_week):
+    """
+    Validate weeks
+    """
+    try:
+        if (int(times_week) < 1 or int(times_week) > 7):
+            raise ValueError(
+                f"Please enter a number between 1 and 7, you entered"
+            )
+    except ValueError as e:
+        print(f"Invalid number: {e}, please try again.\n")
+        return False
+
+    return True
+
+
+def start_menu_calculate_membership(values):
+    """
+    Calculate how much the membership will cost per day
+    """
+    if values == "2":
+        print("We have two memberships. Silver (30€) and Gold (50€)")
+        print("Let's see which membership is best suited for you...")
+        times_week = int(input("How many times per week are you going to train at our gym? "))
+ 
+        #Calculate per month
+        times_month = (times_week * 4)
+        calculate_silver = round(30 / times_month, 2)
+        calculate_gold = round(50 / times_month, 2)
+        print(f"Silver membership will cost you {calculate_silver}€ and gold membership {calculate_gold}€ each time you visit the gym")
+
+        if times_week < 3:
+            print(f"Okay, if you only train {times_week} time per week we recommend you to choose the silver membership.")
+        elif 2 <= times_week <= 4:
+            print(f"Cool, if you train {times_week} times per week we recommend you either the silver or gold membership")
+        elif times_week > 5:
+            print(f"Wow, if you train as much as {times_week} times we recommend you the gold membership")
+
+
 def start_menu_calculate_bmr(values):
     """
     Will let the customer calculate there BMR if they engage in no activity for that day
     """
-    if values == "2":
-        age = int(input("Please enter your age: "))
+    if values == "4":
+        while True:
+            try:
+                age = int(input("Please enter your age: "))
+            except ValueError:
+                print("ERROR, provide your age again")
+                continue
+            else:
+                break
         print(f"Your age is saved as {age}.\n")
-        height = int(input("Please enter your height in (cm): "))
+        while True:
+            try:
+                height = int(input("Please enter your height in (cm): "))
+            except ValueError:
+                print("ERROR, provide your height again")
+                continue
+            else:
+                break
         print(f"Your height is saved as {height}.\n")
-        weight = int(input("Please enter your weight in (kg): "))
+        while True:
+            try:
+                weight = int(input("Please enter your weight in (kg): "))
+            except ValueError:
+                print("ERROR, provide your weight again")
+                continue
+            else:
+                break
         print(f"Your weight is saved as {weight}.\n")
         male_female = input("Please enter (M) for male or (F) for female: ")
 
@@ -117,6 +191,22 @@ def start_menu_calculate_bmr(values):
             bmr = int((10 * weight) + (6.25 * height) - (5 * age) - 161)
         print("Your BMR is " + str(bmr) + ".")
         return calculate_activity(bmr)
+
+
+def validate_activity_level(activity_level):
+    """
+    Validate weeks
+    """
+    try:
+        if (int(activity_level) < 1 or int(activity_level) > 5):
+            raise UnboundLocalError(
+                f"Please enter a number between 1 and 5"
+            )
+    except UnboundLocalError as e:
+        print(f"Invalid number: {e}, please try again.\n")
+        return False
+
+    return True
 
 
 def calculate_activity(bmr):
@@ -157,11 +247,25 @@ def start_menu_calculate_bmi(values):
     """
     # BMI Calculator I took the formula from https://www.thecalculatorsite.com/health/bmicalculator.php
     if values == "3":
-        height = float(input("Please enter your height in (meter): "))
+        while True:
+            try:
+                height = float(input("Please enter your height in (meter): "))
+            except ValueError:
+                print("ERROR, provide your height again")
+                continue
+            else:
+                break
         print(f"Your height is saved as {height}.\n")
-        weight = int(input("Please enter your weight in (kg): "))
+        while True:
+            try:
+                weight = int(input("Please enter your weight in (kg): "))
+            except ValueError:
+                print("ERROR, provide your weight again")
+                continue
+            else:
+                break
         print(f"Your weight is saved as {weight}.\n")
-        #Calculate BMI
+        # Calculate BMI
         bmi_1 = float((weight / height))
         bmi_2 = float((bmi_1 / height))
         round_bmi = round(bmi_2, 2)
@@ -182,31 +286,15 @@ def start_menu_calculate_bmi(values):
             print("A BMI of over 40 indicates that you are very severely obese")        
 
 
-def start_menu_calculate_membership(values):
+def start_menu_manager(values):
     """
-    Calculate
+    Let the staff manager
     """
-    if values == "4":
-        print("We have two memberships. Silver (30€) and Gold (50€)")
-        print("Let's see which membership is best suited for you...")
-        times_week = int(input("How many times per week are you going to train at our gym? "))
-        #Calculate per month
-        times_month = (times_week * 4)
-        calculate_silver = round(30 / times_month, 2)
-        calculate_gold = round(50 / times_month, 2)
-        print(f"Silver membership will cost you {calculate_silver} and gold membership {calculate_gold} each time you visit the gym")
-
-        if times_week < 2:
-            print(f"Okay, if you only train {times_week} time per week we recommend you to choose the silver membership.")
-        elif 2 <= times_week <= 5:
-            print(f"Cool, if you train {times_week} times per week we recommend you either the silver or gold membership")
-        elif times_week > 5:
-            print(f"Wow, if you train as much as {times_week} we recommend you the gold membership")
-
-
-
-
-
+    if values == "5":
+        print("Welcome to the admin portal for staff manager")
+        see_new_customers = input("If you would like to see the 5 newest customers press 1")
+        if see_new_customers == "1":
+            new_customers = SHEET.worksheet("new_customer")
 
 
 def main():
@@ -214,8 +302,6 @@ def main():
     Run all program functions
     """
     start_menu()
-    values = validate_start_menu
-    bmr = start_menu_calculate_bmr(values)
 
 
 main()
